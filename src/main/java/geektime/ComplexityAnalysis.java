1ffa43d9c53e053d0da6dcb4f7cdb00b22aa1a97
malinghan <linghan.ma@gmail.com>
2019-12-23 10:06:04 +0800

    submit

--- src/main/java/geektime/ComplexityAnalysis.java
+++ src/main/java/geektime/ComplexityAnalysis.java
@@ -5,6 +5,22 @@ package geektime;
  * @Package geektime
  * @Description:
  * @date Date : 2019年12月22日 1:50 PM
+ *
+ *
+ * 最好的时间复杂度:  O(1)
+ * 最坏的时间复杂度:  O(n)
+ * 平均的时间复杂度: O(1)
+ * 均摊的时间复杂度: O(1)
+ *
+ * 当i < len时, 即 i = 0,1,2,...,n-1的时候，for循环不走，所以这n次的时间复杂度都是O(1);
+ * 当i >= len时, 即 i = n的时候，for循环进行数组的copy，所以只有这1次的时间复杂度是O(n);
+ * 由此可知:
+ * 该算法的最好情况时间复杂度(best case time complexity)为O(1);
+ * 最坏情况时间复杂度(worst case time complexity)为O(n);
+ * 平均情况时间复杂度(average case time complexity),
+ * 第一种计算方式: (1+1+...+1+n)/(n+1) = 2n/(n+1) 【注: 式子中1+1+...+1中有n个1】,所以平均复杂度为O(1);
+ * 第二种计算方式(加权平均法，又称期望): 1*(1/n+1)+1*(1/n+1)+...+1*(1/n+1)+n*(1/(n+1))=1，所以加权平均时间复杂度为O(1);
+ * 第三种计算方式(均摊时间复杂度): 前n个操作复杂度都是O(1)，第n+1次操作的复杂度是O(n)，所以把最后一次的复杂度分摊到前n次上，那么均摊下来每次操作的复杂度为O(1)
  **/
 public class ComplexityAnalysis {
 
@@ -34,11 +50,6 @@ public class ComplexityAnalysis {
         // 将element放到下标为i的位置，下标i加一
         array[i] = element;
         ++i;
-    }
-
-    public static void main(String[] args) {
-
-
     }
 
 }
