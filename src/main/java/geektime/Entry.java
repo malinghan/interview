0c73c519bdcf2982b0ac7d9bb7d79a4b57adc414
malinghan <linghan.ma@gmail.com>
2019-12-24 15:31:20 +0800

    add

---
+++ src/main/java/geektime/Entry.java
@@ -0,0 +1,14 @@
+package geektime;
+
+/**
+ * @author : linghan.ma
+ * @Package geektime
+ * @Description:
+ * @date Date : 2019年12月23日 7:58 PM
+ **/
+public class Entry {
+    Entry prev;//前一节点
+    Entry next;//后一节点
+    Object value;//值
+    Object key;//键
+}
---
+++ src/main/java/geektime/LRU.java
@@ -0,0 +1,33 @@
+package geektime;
+
+import java.util.LinkedHashMap;
+
+/**
+ * @author : linghan.ma
+ * @Package geektime
+ * @Description:
+ * @date Date : 2019年12月24日 3:10 PM
+ **/
+public class LRU {
+    private final int CAPACITY = 0;
+    private LinkedHashMap<Integer, Integer> cache;
+
+    public LRU(int capacity) {
+
+        cache = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(java.util.Map.Entry<Integer, Integer> eldest) {
+                return size() > CAPACITY;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+
+}
