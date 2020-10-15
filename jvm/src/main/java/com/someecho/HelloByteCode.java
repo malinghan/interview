7ebf34fa3a01a53dc0518377b71a49f30e8709ed
malinghan <linghan.ma@gmail.com>
2020-10-16 00:36:17 +0800

    add class

---
+++ jvm/src/main/java/com/someecho/HelloByteCode.java
@@ -0,0 +1,14 @@
+package com.someecho;
+
+/**
+ * @author : linghan.ma
+ * @Package com.someeecho
+ * @Description:
+ * @date Date : 2020年10月15日 8:37 PM
+ **/
+public class HelloByteCode {
+
+    public static void main(String[] args) {
+        HelloByteCode helloByteCode = new HelloByteCode();
+    }
+}
