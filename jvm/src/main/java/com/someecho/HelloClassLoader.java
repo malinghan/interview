c1786bf393663b3d973561e06b7dfd7ea983f573
malinghan <linghan.ma@gmail.com>
2020-10-18 14:58:27 +0800

    add

--- jvm/src/main/java/com/someecho/HelloClassLoader.java
+++ jvm/src/main/java/com/someecho/HelloClassLoader.java
@@ -1,7 +1,6 @@
 package com.someecho;
 
 import java.io.*;
-import java.util.Base64;
 
 /**
  * @author : linghan.ma
@@ -30,24 +29,9 @@ public class HelloClassLoader extends ClassLoader{
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         File file = new File("/Users/malinghan/IdeaProjects/learning-space/interview/jvm/src/main/java/com/someecho/Hello.xlass");
-//        File file = new File("/Users/malinghan/IdeaProjects/learning-space/interview/jvm/src/main/java/com/someecho/MovingAverage.class");
-
         byte[] bytes = readString1(file);
         System.out.println(bytes);
-//        String helloS = Base64.getEncoder().encodeToString(helloBase64);
-//        String helloS = new String(helloBase64);
-//        System.out.println(helloBase64);
-//        byte[] bytes = decode(helloBase64);
-//        byte[] bytes = readString2(file);
         return defineClass(name,bytes,0,bytes.length);
-    }
-
-    public byte[] encode(byte[] base64){
-        return Base64.getEncoder().encode(base64);
-    }
-
-    public byte[] decode(String base64){
-        return Base64.getDecoder().decode(base64);
     }
 
     /* 貌似是说网络环境中比较复杂，每次传过来的字符是定长的，用这种方式？*/
